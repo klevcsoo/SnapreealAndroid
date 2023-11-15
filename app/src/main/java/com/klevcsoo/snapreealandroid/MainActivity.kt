03d0b6182d9b2d7ab65aeaf3e94d0444ece10752
Keve Lajsz <kevelajsz@gmail.com>
2023-11-15 21:05:59 +0100

    initial commit

---
+++ app/src/main/java/com/klevcsoo/snapreealandroid/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.klevcsoo.snapreealandroid
+
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Creates a button that mimics a crash when pressed
+        // Creates a button that mimics a crash when pressed
+        val crashButton = Button(this)
+        crashButton.text = "Test Crash"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(
+            crashButton, ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        )
+    }
+}
