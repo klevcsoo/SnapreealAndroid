4ee28c52fd576735787dd1eef8a211216cea8e3f
Keve Lajsz <kevelajsz@gmail.com>
2023-12-05 13:57:56 +0100

    refs #4 remove auth and firebase

--- app/src/main/java/com/klevcsoo/snapreealandroid/diary/ui/list/DiaryListViewModel.kt
+++ app/src/main/java/com/klevcsoo/snapreealandroid/diary/ui/list/DiaryListViewModel.kt
@@ -4,15 +4,10 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.klevcsoo.snapreealandroid.auth.UserRepository
 import com.klevcsoo.snapreealandroid.diary.DiaryRepository
 import com.klevcsoo.snapreealandroid.diary.model.DiaryModel
-import com.klevcsoo.snapreealandroid.service.FirebaseService
 
 class DiaryListViewModel : ViewModel() {
-    private val auth = FirebaseService.instance.auth
-
-    private val userRepository = UserRepository()
     private val diaryRepository = DiaryRepository()
 
     private val _diaries = MutableLiveData<List<DiaryModel>>(listOf())
